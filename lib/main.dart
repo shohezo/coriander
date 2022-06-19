fd33e488a2cbfb265c51e48694b6e65e82297b3a
shohezo <s08052346550@gmail.com>
2022-06-19 14:16:44 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:coriander/main_model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('コリアンダー'),
+          ),
+          body: Consumer<MainModel>(builder: (context, model, child) {
+            return Center(
+              child: Column(
+                children: [
+                  Text(
+                    model.kboyText,
+                    style: TextStyle(fontSize: 30),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      model.changeKboyText();
+                    },
+                    child: Text('ボタン'),
+                  )
+                ],
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
